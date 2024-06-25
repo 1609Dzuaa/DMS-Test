8f99923fcfde05d27da58da43ad23a10857c7a1d
1609Dzuaa <106080372+1609Dzuaa@users.noreply.github.com>
2024-06-25 10:17:20 +0700

    import Dotween package, handle UI HP

--- Assets/Scripts/Base/StateMachine/BaseCharacter.cs
+++ Assets/Scripts/Base/StateMachine/BaseCharacter.cs
@@ -1,21 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class BaseCharacter : BaseObject
 {
     #region !PUBLIC FIELDS
     
     protected CharacterBaseState _state;
+    protected float _baseHP;
 
     [Header("HP")]
-    [SerializeField] protected float _healthPoint; 
+    [SerializeField] protected float _healthPoint;
+
+    [Header("HP UI Ref")]
+    [SerializeField] protected Image _hpFill; //Thanh máu để fill
+    [SerializeField] protected GameObject _hpBar; //Gobj chứa thanh máu
+    [SerializeField] protected float _fillSpeed; 
 
     #endregion
 
     #region PUBLIC FIELDS
 
     public float HealthPoint { get => _healthPoint; set => _healthPoint = value; }
+
+    public GameObject HPBar { get => _hpBar; set => _hpBar = value; }
 
     #endregion
 
@@ -26,15 +36,38 @@ public class BaseCharacter : BaseObject
         _state.EnterState(this);
     }
 
+    protected override void SetUpProperties()
+    {
+        _baseHP = _healthPoint;
+    }
+
     protected override void Update()
     {
         _state?.Update();
+        HanleHPBarRotation();
     }
 
     protected virtual void FixedUpdate()
     {
         _state?.FixedUpdate();
     }
+
+    protected void HanleHPBarRotation()
+    {
+        //Cố định 0 cho thanh hp rotate khi parent object rotate
+        if (_hpBar != null)
+            if (_hpBar.transform.eulerAngles.y > 0)
+                _hpBar.transform.eulerAngles = new(_hpBar.transform.eulerAngles.x, 0f, _hpBar.transform.eulerAngles.z);
+    }
+
+    protected void HandleHealthPoint(float damageTaken)
+    {
+        _healthPoint -= damageTaken;
+        Mathf.Clamp(_healthPoint, 0f, _baseHP);
+        _hpFill.DOFillAmount(_healthPoint / _baseHP, _fillSpeed);
+    }
+
+    #region Animation Events
 
     //Event của animation attack wakeup rb khi attack tránh TH:
     //Player và quái đứng yên nhưng kh bắt đc trigger collision
@@ -48,4 +81,6 @@ public class BaseCharacter : BaseObject
     {
         _rb.Sleep();
     }
+
+    #endregion
 }
